007ba8b55c21fc1676aa667b86e076a8da1869f5
agentlans <at92001@gmail.com>
2022-11-26 20:52:10 -0500

    More control structures

--- extra/control.scm
+++ extra/control.scm
@@ -1,5 +1,10 @@
 (define-module (extra control)
-  #:export (dotimes))
+  #:export (
+  dotimes
+  for
+  do-until
+  infinite-loop
+  ))
 
 ;; Control structures not found in standard Scheme
 
@@ -7,3 +12,26 @@
   (do ((var 0 (+ var 1)))
       ((= var n))
     body...))
+
+(define-syntax-rule (for (var start condition incr-expr) body ...)
+  (do ((var start incr-expr))
+    ((not condition))
+    (begin body ...)))
+
+#| ;; Guile already has this
+(define-syntax-rule (while condition body ...)
+  (let loop ()
+    (when condition
+      (begin body ...)
+      (loop))))
+|#
+
+(define-syntax-rule (do-until condition body ...)
+  (let loop ()
+    (begin body ...)
+    (unless condition
+      (loop))))
+
+(define-syntax-rule (infinite-loop body ...)
+  (while #t (begin body ...)))
+
